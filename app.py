74360047359f70b0f2dd69922ba705543e99c983
plamy-aughhh <pdstoyankova24a@gmail.com>
2026-03-16 12:36:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ st.write("Enter a book title to see if it exists in the database")
 user_input = st.text_input ("Book Title")
 
 if st.button("Check Box"):
-  if user_input in books:
+  if user_input.strip() == "":
+    st.warning("Please enter a book title")
+  elif user_input in books:
     st.success("The book is in the database! ")
   else:
     st.error("The book is not in  the database. ")
